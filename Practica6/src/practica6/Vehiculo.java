ac1b64e61ca1c8103128387c2c2b017ebb657760
LidiaCuadro <50886899+LidiaCuadro@users.noreply.github.com>
2019-06-10 16:32:42 +0200

    #5 Refactorización del código #7 Javadoc

---
+++ Practica6/src/practica6/Vehiculo.java
@@ -0,0 +1,18 @@
+package practica6;
+
+/**
+ * Clase Vehiculo.
+ * @author Lidia Cuadro Pérez
+ *
+ */
+public class Vehiculo {
+	String matricula;
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+}
